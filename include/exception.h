3a9cb8a8b88b55b952f2f887be716eb3b993edc8
jon-tow <jonathantow1@gmail.com>
2019-11-10 17:18:16 -0500

    Add `invalid_arg_size exception`

--- include/exception.h
+++ include/exception.h
@@ -20,4 +20,16 @@ class ill_form_error : public std::runtime_error {
         }
 };
 
+class invalid_arg_size : public std::runtime_error {
+      public:
+        invalid_arg_size(const std::string& name, size_t size,
+                         size_t expected_size)
+            : std::runtime_error("\n; " + name + " has been called with " +
+                                 std::to_string(size) +
+                                 " arguments; it requires " +
+                                 std::to_string(expected_size) + ".\n")
+        {
+        }
+};
+
 #endif // EXCEPTION_H
